98a50db1f6e76fe0f74b218fb5a6cd236145ec64
Jesus del toro robledo <jesusdeltoror@gmail.com>
2023-02-03 11:29:48 -0600

    gff

--- js/index.js
+++ js/index.js
@@ -53,7 +53,7 @@ facebook.addEventListener('click',() => {
             // This gives you a Facebook Access Token. You can use it to access the Facebook API.
             const credential = FacebookAuthProvider.credentialFromResult(result);
             const accessToken = credential.accessToken;
-
+            console.log("conectado");
             // ...
         })
         .catch((error) => {
@@ -64,7 +64,7 @@ facebook.addEventListener('click',() => {
             const email = error.customData.email;
             // The AuthCredential type that was used.
             const credential = FacebookAuthProvider.credentialFromError(error);
-
+            console.log(errorMessage);
             // ...
         });
 })
